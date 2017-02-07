6d9a4e18d5177da5abc4af2ba0bba2b10e60d578
dr-matt-smith <dr_matt_smith@me.com>
2017-02-07 12:36:34 +0000

    basic with controller classes

--- public/index.php
+++ public/index.php
@@ -1,2 +1,10 @@
 <?php
 require_once __DIR__ . '/../vendor/autoload.php';
+
+
+$app = new Silex\Application();
+
+$app->get('/', 'Itb\MainController::indexAction');
+$app->get('/about', 'Itb\MainController::aboutAction');
+
+$app->run();
--- src/MainController.php
+++ src/MainController.php
@@ -5,5 +5,15 @@ namespace Itb;
 
 class MainController
 {
+    public function indexAction()
+    {
+        return 'hello from the home page';
+    }
+
+    public function aboutAction()
+    {
+        return 'about page - welcome to My Great Website';
+    }
+
 
 }
